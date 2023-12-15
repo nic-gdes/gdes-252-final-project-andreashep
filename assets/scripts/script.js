63c486270c83d8c31b437e6f17a7b76da2002fdf
Andrea Shepherd <118775368+andreashep@users.noreply.github.com>
2023-12-14 18:00:15 -0800

    filled out functions, connected with js, animations, now I'm just tired........

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,51 +1,42 @@
-/*// switch button for dark mode
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const container = document.querySelector('.container');
-toggleSwitch.addEventListener('change', function () {
-    if (this.checked) {
-        container.classList.add('dark');
-    } else {
-        container.classList.remove('dark');
-    }
-});*/
-const themeToggle = document.getElementById("theme-toggle");
-            const body = document.body;
-
-            themeToggle.addEventListener("change", () => {
-                body.classList.toggle("dark");
-            });
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
 //conversion tool
-function convert(type) {
-    const inputNumber = document.getElementById("inputNumber");
-    
-    let result;
-  
-    switch (type) {
-      case 'fahrenheit':
-        result = (inputNumber - 32) * (5/9);
-        break;
-      case 'feet':
-        result = inputNumber * 0.000189394; // 1 foot = 0.000189394 miles
-        break;
-      case 'inches':
-        result = inputNumber * 2.54; // 1 inch = 2.54 centimeters
-        break;
-      default:
-        result = 'Invalid conversion type';
-    }
-    if (!isNaN(result)) {
-        document.getElementById('outputResult').value = result.toFixed(2);
-      } else {
-        alert('Please enter a valid number.');
-      }
-   
-   
-    const resetButton = document.getElementById("resetButton");
 
-    function resetButton() {
-        document.getElementById("inputNumber").value = "";
-        document.getElementById("outputResult").value = "";
-    }
-    // Reset input field
-    };
+//inoutNumberText
+function convert(type) {
+  const inputNumber = parseFloat(document.getElementById('inputNumberText').value);
+  let result;
+
+  switch (type) {
+    case 'fahrenheit':
+      result = (inputNumber - 32) * (5/9);
+      break;
+    case 'feet':
+      result = inputNumber * 0.000189394; // 1 foot = 0.000189394 miles
+      break;
+    case 'inches':
+      result = inputNumber * 2.54; // 1 inch = 2.54 centimeters
+      break;
+    default:
+      result = 'Invalid conversion type';
+  }
   
+  document.getElementById('modal-overlay').classList.add('active');
+  
+  // Display result
+  if (!isNaN(result)) {
+    document.getElementById('outputResultText').value = result.toFixed(2);
+  } else {
+    alert('Please enter a valid number.');
+  }
+}
+
+function reset() {
+  document.getElementById('inputNumberText').value = '';
+  document.getElementById('outputResultText').value = '';
+  document.getElementById('inputNumberText').focus();
+
+  document.getElementById('modal-overlay').classList.remove('active');
+}
